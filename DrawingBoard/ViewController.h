823772e2ed5d157779524eb03121452d382cc681
mattilla <mattilla@163.com>
2015-03-13 15:46:04 +0800

    set up

---
+++ DrawingBoard/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  DrawingBoard
+//
+//  Created by Matthew on 3/13/15.
+//  Copyright (c) 2015 Matthew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//
+//@property (weak,nonatomic)UIColor *currentColor;
+
+@end
+
